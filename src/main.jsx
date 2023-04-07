4bcd078555389fdcf3872b70d31086e24144511a
FedorVinecky <bostbotbeet@gmail.com>
2023-04-07 12:15:46 +0400

    scroll top react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 import {BrowserRouter} from 'react-router-dom'
+import ScrollToTop from './helpers/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
 )
